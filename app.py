86120eeb89b584fe5152bbaa6f021b128c2e3291
Andy Baek <andybaek95@gmail.com>
2018-08-01 01:01:58 -0400

    Add Movie endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
 import os
@@ -7,47 +7,37 @@ import pymysql
 app = Flask(__name__)
 CORS(app)
 
-sample_data = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+def establish_conn():
+    return pymysql.connect(
+        db='example',
+        user='root',
+        passwd='hello123',
+        host='localhost')
 
 @app.route("/")
 def hello():
     print("Hi!")
     return "Hello World!"
 
-@app.route('/data', methods=['GET'])
-def get_data():
-    print("get_data called")
-    return jsonify({'movies': sample_data})
-
 @app.route('/movies', methods=['GET'])
 def get_movies():
-    conn = pymysql.connect(
-        db='example',
-        user='root',
-        passwd='hello123',
-        host='localhost')
+    conn = establish_conn()
     c = conn.cursor()
     c.execute("SELECT * FROM movies")
     movies = [{"title": r[0], "rating": r[1]} for r in c.fetchall()]
     return jsonify(movies)
 
-#    print("get_movies_called")
-#    with open('movies.json', 'r') as f:
-#        movies = json.load(f)
-#    return jsonify(movies)
+@app.route('/add_movie', methods=['POST'])
+def add_movie(): 
+    data = request.get_json()
+    title = data['title']
+    rating = int(data['rating'])
+
+    conn = establish_conn()
+    c = conn.cursor()
+    c.execute("INSERT INTO movies VALUES ('{}', {})".format(title.replace("'", "''"), rating))
+    conn.commit()
+    return "Success adding {}".format(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True)
